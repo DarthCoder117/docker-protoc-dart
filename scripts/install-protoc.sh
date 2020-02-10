446992538f1817cf3dc454f2b0a5756db9fbeeb9
Tanner Mickelson <tanner@icicle-games.com>
2020-02-10 23:53:16 +0900

    Add build script to install protoc

---
+++ scripts/install-protoc.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+VERSION="3.11.3"
+
+BASE_URL="https://github.com/protocolbuffers/protobuf/releases/download"
+PLATFORM="$(uname -s)"
+ARCH="$(uname -m)"
+
+PROTOC_URL="${BASE_URL}/v${VERSION}/protoc-${VERSION}-${PLATFORM}-${ARCH}.zip"
+
+echo "Downloading protoc from: $PROTOC_URL"
+curl -sSL -o protoc_temp.zip ${PROTOC_URL}
+
+echo "Extracting protoc to /usr/local"
+unzip protoc_temp.zip -d /usr/local
+rm protoc_temp.zip
